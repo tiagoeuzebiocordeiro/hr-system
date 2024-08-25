9aabed993bf9cbbeccbd6ec6c4d349caca180808
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-08-25 18:54:44 -0300

    EMPLOYEE Registered Client for Eureka Server + Add Log (Sl4j) at Service

--- employee/src/main/java/com/tiagocordeiro/employee/services/impl/EmployeeServiceImpl.java
+++ employee/src/main/java/com/tiagocordeiro/employee/services/impl/EmployeeServiceImpl.java
@@ -4,21 +4,26 @@ import com.tiagocordeiro.employee.domain.Employee;
 import com.tiagocordeiro.employee.repositories.EmployeeRepository;
 import com.tiagocordeiro.employee.services.EmployeeService;
 import com.tiagocordeiro.employee.services.exceptions.ObjectNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
     private EmployeeRepository repository;
-
+    @Autowired
+    private Environment env;
     @Override
     @Transactional(readOnly = true)
     public Employee findById(Long id) {
+        log.info("EMPLOYEE_SERVICE ::: Get Request on {} port", env.getProperty("local.server.port"));
         return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException(String.format(
                 "Employee of id=%s not found", id
         )));
